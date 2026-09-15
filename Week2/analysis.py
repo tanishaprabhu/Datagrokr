038dbeda53c1ae66b4aa4507c7e5ab4d7514749e
tanishaprabhu <tanishaprabhu0605@gmail.com>
2026-09-15 16:21:31 +0530

    Update analysis.py

--- Week2/analysis.py
+++ Week2/analysis.py
@@ -1,19 +1,11 @@
 import pandas as pd
-
-
 try:
-
     df = pd.read_csv("all_transactions.csv")
 
     print("\n ALL TRANSACTIONS ")
     print(df.to_string(index=False))
-
-
-    
     deposits = df[df["Type"] == "Deposit"]
     withdrawals = df[df["Type"] == "Withdrawal"]
-
-
     print("\n OVERALL ANALYSIS ")
 
     print("Total Deposited:",
@@ -28,7 +20,6 @@ try:
     print("Number of Withdrawals:",
           len(withdrawals))
 
-
     if len(deposits) > 0:
         print("Average Deposit:",
               deposits["Amount"].mean())
@@ -37,8 +28,6 @@ try:
         print("Average Withdrawal:",
               withdrawals["Amount"].mean())
 
-
-    # Account-wise analysis
     print("\nACCOUNT-WISE ANALYSIS")
 
     account_summary = df.groupby(
@@ -47,9 +36,7 @@ try:
 
     print(account_summary)
 
-
 except FileNotFoundError:
-
     print("all_transactions.csv not found.")
     print("First run bank_account.py and choose")
     print("'Save All Transactions'.")
